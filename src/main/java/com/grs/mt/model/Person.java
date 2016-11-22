aba875d3f35c127ba33470468fed514f5ecae2e1
dwsmithson <dwsmithson@gmail.com>
2016-11-21 20:24:47 -0700

    Additional fields and relationship establishment.

---
+++ src/main/java/com/grs/mt/model/Person.java
@@ -0,0 +1,48 @@
+package com.grs.mt.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by dwsmithson on 11/21/16.
+ */
+@Entity
+public class Person {
+
+    @OneToMany(mappedBy = "person")
+    private Set<Task> tasks = new HashSet<>();
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+
+    Person() {}
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+
+}
