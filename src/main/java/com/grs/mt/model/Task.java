4b3282920efe34b742ff9665cd6251e905549478
dwsmithson <dwsmithson@gmail.com>
2016-11-21 20:25:02 -0700

    Additional fields and relationship establishment.

--- src/main/java/com/grs/mt/model/Task.java
+++ src/main/java/com/grs/mt/model/Task.java
@@ -1,15 +1,43 @@
 package com.grs.mt.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 /**
  * Created by dwsmithson on 11/13/16.
  */
 @Entity
-public class Task {
+class Task {
+
+    @JsonIgnore
+    @ManyToOne
+    private Person person;
 
     @Id
+    @GeneratedValue
     private long id;
+
     private String name;
+
+    private Task() {}
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
